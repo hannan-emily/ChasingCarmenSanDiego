38d5c53ddae0d009acc0d7c43021144774ed0678
Emily Hannan <emilyhannan@Emilys-MacBook-Air.local>
2018-02-05 10:54:45 -0800

    built initial file structure of html css js

---
+++ js/data.js
@@ -0,0 +1,61 @@
+//
+// question 1: "a witness reported seeing carmen while touring Fort Jesus, a former portguese prison from the 16th century. what island was she on?"
+// correct answer: mombasa island
+// incorrect answer: lisbon
+// incorrect answer: buenos aires
+// incorrect answer: auckland
+//
+// question 2: "a security guard spotted a woman clutching blueprints at the southwest corner of the Cristo Redentor statue in this city. where was carmen, if this was her?"
+// correct answer: sao paolo
+// incorrect answer: buenos aires
+// incorrect answer: lisbon
+// incorrect answer: pittsburgh
+//
+// question 3: "a bus full of tourists claimed carmen nearly ran them off the Hana highway, which has 59 bridges, while on this island. is she on:"
+// correct answer: Maui
+// incorrect answer: Mombasa island
+// incorrect answer: falkland islands
+// incorrect answer: hong kong
+//
+// question 4: "a elderly gentlemen reported a suspicus woman aboard the 'Peak Tram', as he rode this funincular train to view this city from Victoria Peak"
+// correct answer: Hong Kong
+// incorrect answer: auckland
+// incorrect answer: pittsburgh
+// incorrect answer: sao paolo
+//
+// question 5: "while touring this area to view all of it's 446 bridges, an off duty firefighter may have seen Carmen on top of the Libery Bridge"
+// correct answer: Pittsburgh
+// incorrect answer: maui
+// incorrect answer: sao paulo
+// incorrect answer: hong kong
+//
+//
+// question 6: "while touring the smallest cricket stadium in world, Eden Park, this teenager thought he saw Carmen slinking away. what city were they in"
+// correct answer: Auckland
+// incorrect answer: helsinki
+// incorrect answer: buenos aires
+// incorrect answer: sao paulo
+//
+// question 7: "a child thought she saw carmen attempting to pry off a mural from the Pope's Church (also known as the Metropolitan Catherdal) in this area"
+// correct answer: Buenos Aires
+// incorrect answer: Sao Paolo
+// incorrect answer: maui
+// incorrect answer: pittsburgh
+//
+// question 8: "a former army captain thought he saw carmen atttmeping to steal the Hellisheidarvirkjun geothermal power plant plans. what location does this power?"
+// incorrect answer: helsinki
+// incorrect answer: falkland islands
+// incorrect answer: Maui
+// inocrrect answer: mombasa island
+//
+// question 9: "carmen was possibly spotted smuggling whale oil in this disputed terriotory, still both claimed as part of the UK and Argentina"
+// incorrect answer: falkland islands
+// incorrect answer: helsinki
+// inocrrect answer: buenos Aires
+// inocrrect answer: lisbon
+//
+// question 10: "riding an old fashioned wooden funicular train up this city's cobbled streets, carmen was potenitally recognized by a former child actor in this locale"
+// incorrect answer: lisbon
+// incorrect answer: mombasa island
+// inocrrect answer: hong kong
+// inocrrect answer: auckland
