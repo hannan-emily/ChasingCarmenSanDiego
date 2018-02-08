bb3e0985e601c505377b20b925df396b6a208ace
hannan-emily <emilyhannan@gmail.com>
2018-02-07 18:54:12 -0800

    questions now only display once, and then another displays. the turncount is linked to the question array. the turncount advances by 1 each time the question modal is closed, so when the modal is opened the next time, a new question displays.

--- js/app.js
+++ js/app.js
@@ -11,73 +11,85 @@ var questionArray = [{
   question: "An optimistic but unsuccessful metal detectorist spotted someone who looked like Carmen entering 'The Cave of the Crystals', home to the world's of the largest salt crystals. If she is stealing crystals, where is she?",
   correctAnswer: 'Naica, Mexico',
   incorrectAnswers: ['Ashgabat, Turkmenistan','Falkland Islands','Pittsburgh, United States'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "A bored security guard briefly spotted a woman clutching blueprints at the southwest corner of the Cristo Redentor statue. Where did this sighting happen?",
   correctAnswer: 'Sao Paolo, Brazil',
   incorrectAnswers: ['Naica, Mexico','Geneva, Switzerland','Falkland Islands'],
   answers:[],
+  carmen:[]
 },
 {
   question: "While attempting to visit all 446 bridges in 1 day, an off duty firefighter may have seen Carmen trying to steal the historic bolts off The Liberty Bridge. Where was this incident?",
   correctAnswer: 'Pittsburgh, United States',
   incorrectAnswers: ['Okrug, Russia','Sao Paolo, Brazil','Ashgabat, Turkmenistan'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "Right after pulling out of this train station, the second engineer on the world's oldest steam locomotive, 'The Fairy Queen', thought he saw Carmen attempting to pry valuable engine parts loose. If Carmen really was stealing parts, what city did that train just leave?",
   correctAnswer: 'Alwar, India',
   incorrectAnswers: ['Reykjavik, Iceland','Naica, Mexico','Okrug, Russia'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "A former army captain thought he saw Carmen atttemping to steal plans from the control room of the Hellisheidarvirkjun geothermal power plant. What city does this power?",
   correctAnswer: 'Reykjavik, Iceland',
   incorrectAnswers: ['Geneva, Switzerland','Ashgabat, Turkmenistan','Moscow, Russia'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "This southern hemisphere locale is still officially claimed by two different countries. A fishmonger might have spotted Carmen attempting to fence 1 ton of whale blubber. Where was she trying to make a deal?",
   correctAnswer: 'Falkland Islands',
   incorrectAnswers: ['Sao Paolo, Brazil','Alwar, India','Grootfontein, Namibia'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "Two seperate unnamed witnesses reportedly saw Carmen chip away at the 69 foot, solid gold statue of their president (atop a solid golid horse). If she stole that gold, where was this statue?",
   correctAnswer: 'Ashgabat, Turkmenistan',
   incorrectAnswers: ['Moscow, Russia','Nevsehir Province, Turkey','Pittsburgh, United States'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "A group of geologists visiting the 'Monument to the Conquerors of Space' may have witnessed Carmen attempting to pull the 351 foot titanium sculpture away with a rusty crane. She was unsuccessful. Where was this?",
   correctAnswer: 'Moscow, Russia',
   incorrectAnswers: ['Nevsehir Province, Turkey','Pittsburgh, United States','Sao Paolo, Brazil'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "While inspecting a leaking pipe deep underground in the Hadron Collider, a physiscit may have seen Carmen shuffling away with several laser scopes. What city were they both underneath?",
   correctAnswer: 'Geneva, Switzerland',
   incorrectAnswers: ['Alwar, India','Reykjavik, Iceland','Moscow, Russia'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "Spotted at night by a amature hot air balloon enthusisast, Carmen may have been digging up the world's largest meteroite: 'The Hoba'. Where did he spot her?",
   correctAnswer: 'Grootfontein, Namibia',
   incorrectAnswers: ['Falkland Islands','Naica, Mexico','Okrug, Russia'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "On a night shift, a janitor in the world's smallest power plant 'Bilibino Nuclear' thought he saw Carmen slithering out through an air vent. Where is this nuclear plant?",
   correctAnswer: 'Okrug, Russia',
   incorrectAnswers: ['Nevsehir Province, Turkey','Moscow, Russia','Reykjavik, Iceland'],
-  answers:[]
+  answers:[],
+  carmen:[]
 },
 {
   question: "During an ironic throwback rave in the underground city Derinkuyu, a slightly dazed student thought he saw Carmen stashing a mysterious battered trunk. What above-ground city is this ruin under?",
   correctAnswer: 'Nevsehir Province, Turkey',
   incorrectAnswers: ['Grootfontein, Namibia','Alwar, India','Naica, Mexico'],
-  answers:[]
+  answers:[],
+  carmen:[]
 }];
 
 // 100 to 1000
@@ -146,6 +158,8 @@ var questionBoxes = [
   },
 ];
 
+var turnCount = 0;
+
 //CANVAS ELEMENT THAT INTERACTS WITH QUESTIONBOX ELEMENTS
 var detectiveBox = {
   x: 650,
@@ -175,11 +189,20 @@ var shuffleAnswers = function() {
   });
 }
 
-//display selected elements for this question
+//SET CARMEN CONDITION TO TRUE (THE END GAME WIN CONDITION) FOR ONE RANDOM QUESTI0N
+var addCarmenClass = function() {
+  var randomIndex = Math.floor((Math.random() * 11) + 1); // generate a random number for one of the 12 objects (0-11 in array)
+  console.log(randomIndex);
+  questionArray[randomIndex].carmen.push(true); //now add the winning class of carmen to the object at that index number
+  console.log(questionArray);
+  }
+
+//DISPLAY QUESTIONS & ANSWERS
 var displayQuestion = function() {
-  $("#currentQuestion").text(questionArray[0].question); //replace the text in html field with THIS question text
+  $("#currentQuestion").text(questionArray[turnCount].question); //replace the text in html field with THIS question text
   $("#answer" + 1 + "Text").html(questionArray[0].correctAnswer);
-  answers();
+  // answers();
+  // addCarmenClass();
   // console.log(questionArray);
   // shuffleAnswers();
   // console.log(questionArray)
@@ -192,12 +215,15 @@ var displayQuestion = function() {
 //modal for the question array. this appends an "active" class to .question-overlay and .question-content when the "Open" button is clicked
 var openModal = function(){
  $(".question-overlay, .question-content").addClass("active");
- displayQuestion(); //call the question data& display in this modal
+ displayQuestion();
+ answers();
+ addCarmenClass();
 };
 
 //removes the "active" class off the modal when the "Close" button is clicked
 $("#info-close").on("click", function(){
   $(".question-overlay, .question-content").removeClass("active");
+  turnCount++;
 });
 
 var crash = false;
@@ -221,6 +247,7 @@ var collisonCheck = function(question){
       crash = false;
       detectiveBox.x = 650;
       detectiveBox.y = 350;
+      console.log(turnCount);
       openModal();
     }
   }
